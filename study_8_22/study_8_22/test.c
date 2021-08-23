b3783ec7d316bca6d2ad373d8d6941019db0f5d7
Yellow_UY <26238112@qq.com>
2021-08-23 12:36:44 +0800

    函数指针数组指针

--- study_8_22/study_8_22/test.c
+++ study_8_22/study_8_22/test.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
 //int main()
@@ -107,4 +108,188 @@
 ////简化
 //typedef void(*ptf_1)(int);//类型简化
 //ptf_1 signal(int,prf_1);
+
+//int Add(int x, int y)
+//{
+//    return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//    return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//    return x * y;
+//}
+//int Div(int x, int y)
+//{
+//    return x / y;
+//}
+//int main()
+//{
+//    //指针数组
+//    int a = 10;
+//    int b = 20;
+//    int* arr[5];
+//    //需要一个数组，这个数组可以存放4个函数的地址-函数指针的数组
+//    int(*p)(int, int) = Add;//Sub Mul Div
+//    int(*parr[4])(int, int) = { Add,Sub,Mul,Div }; //函数指针的数组
+//    int i = 0;
+//    for (i = 0; i < 4; i++)
+//    {
+//        printf("%d\n", parr[i](2, 3));
+//    }
+//    return 0;
+//}
+//char* (*pf)(char*, const char*)
+//char* (*pfarr[4])(char*, const char*);
+//计算器
+void menu()
+{
+    printf("*******************************\n");
+    printf("**1.Add*****************2.Sub**\n");
+    printf("**1.Mul*****************2.Div**\n");
+    printf("********     0.Exit    ********\n");
+    printf("*******************************\n");
+}
+int Add(int x, int y)
+{
+    return x + y;
+}
+int Sub(int x, int y)
+{
+    return x - y;
+}
+int Mul(int x, int y)
+{
+    return x * y;
+}
+int Div(int x, int y)
+{
+    return x / y;
+}
+//int main()
+//{
+//    int input = 0;
+//    int x = 0;
+//    int y = 0;
+//    do
+//    {
+//        menu();
+//        printf("请选择:>");
+//        scanf("%d", &input);
+//      
+//        switch (input)
+//        {
+//        case 1:
+//            printf("请输入两个操作数:>");
+//            scanf("%d%d", &x, &y);
+//            printf("%d\n", Add(x, y));
+//            break;
+//        case 2:
+//            printf("请输入两个操作数:>");
+//            scanf("%d%d", &x, &y);
+//            printf("%d\n", Sub(x, y));
+//            break;
+//        case 3:
+//            printf("请输入两个操作数:>");
+//            scanf("%d%d", &x, &y);
+//            printf("%d\n", Mul(x, y));
+//            break;
+//        case 4:
+//            printf("请输入两个操作数:>");
+//            scanf("%d%d", &x, &y);
+//            printf("%d\n", Div(x, y));
+//            break;
+//        case 0:
+//            printf("退出程序\n");
+//            break;
+//        default:
+//            printf("选择错误\n");
+//            break;
+//        }
+//    } while (input); 
+//}
+//int main()
+//{
+//    int input = 0;
+//    int x = 0;
+//    int y = 0;
+//    int (*pfArr[5])(int, int) = {0,Add,Sub,Mul,Div};//pfArr是一个函数指针数组-转移表
+//    do
+//    {
+//        menu();
+//        printf("请选择:>");
+//        scanf("%d", &input); 
+//        if (input >= 1 && input <= 4)
+//        {
+//            printf("请输入两个操作数:>");
+//            scanf("%d%d", &x, &y);
+//            int ret = pfArr[input](x, y);
+//            printf("%d\n", ret);
+//        }
+//        else if (input == 0)
+//        {
+//            printf("退出\n");
+//        }
+//        else
+//        {
+//            printf("选择错误\n");
+//        }
+//    } while (input);
+//    return 0;
+//}
+//回调函数-----------就是一个通过函数指针调用的函数
+//void Calc(int (*pf)(int,int))
+//{
+//    int x = 0;
+//    int y = 0;
+//    printf("请输入两个操作数:>");
+//    scanf("%d%d", &x, &y);
+//    printf("%d\n", pf(x, y));
+//}
+//int main()
+//{
+//    int input = 0;
+//    int x = 0;
+//    int y = 0;
+//    do
+//    {
+//        menu();
+//        printf("请选择:>");
+//        scanf("%d", &input);
+//      
+//        switch (input)
+//        {
+//        case 1:
+//            Calc(Add);
+//            break;
+//        case 2:
+//            Calc(Sub);
+//            break;
+//        case 3:
+//            Calc(Mul);
+//            break;
+//        case 4:
+//            Calc(Div);
+//            break;
+//        case 0:
+//            printf("退出程序\n");
+//            break;
+//        default:
+//            printf("选择错误\n");
+//            break;
+//        }
+//    } while (input); 
+//}
+
+//指向函数指针数组的指针
+
+//函数指针
+//int(*p)()
+//函数指针数组
+//int (*pArr[])()
+// ppArr=&pArr
+// int (*（*ppArr）[])()
+//函数指针数组指针
 
